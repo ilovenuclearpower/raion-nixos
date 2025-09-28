9c0c133462e4afa75eb9b08f48a66c753120fb29
hik <hik@aboveaverage.space>
2025-09-28 12:27:11 -0700

    chore: formatted stylix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stylix.url = {"github:danth/stylix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    stylix = { url = "github:danth/stylix/release-25.05"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
